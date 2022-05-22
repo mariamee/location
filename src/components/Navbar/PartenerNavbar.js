81a7e7d2462dcffa39192c351916862f9744073b
mariamee <mariameelalaoui@gmail.com>
2022-05-22 14:23:51 +0100

    modify navbar

--- src/components/Navbar/PartenerNavbar.js
+++ src/components/Navbar/PartenerNavbar.js
@@ -13,11 +13,15 @@ const PartnerNavbar = () => {
   }
 
   return (
-    <nav className="  d-flex  p-2 ml-2 shadow navbar navbar-expand-xl navbar-light bg-warning ">
+    <div className="  d-flex  justify-content-start align-items-center p-2 shadow navbar navbar-expand-xl navbar-light bg-dark ">
       <div className="container-fluid h6 justify-content-start">
         <div className="dd">
-          <Link className="navbar-brand" to="personal-information">
+          <Link className="navbar-brand text-light" to="personal-information">
             <img src="https://i.imgur.com/ngIERO2.png" alt="profile" />
+            <Link className="navbar-brand-warning " to="personal-information">
+              Mon{' '}
+            </Link>{' '}
+            profil
           </Link>
         </div>
         <div className="gg">
@@ -36,38 +40,36 @@ const PartnerNavbar = () => {
         <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="myadverts">
-                <Link className="navbar-brand" to="/">
-                  Acceuil
-                </Link>
-              </a>
+              <Link className="nav-link text-light" to="/">
+                Acceuil
+              </Link>
             </li>
-            {/* <li className="nav-item">
-              <a className="nav-link" href="mymessages">
+            {/* {<li className="nav-item">
+              <a className="nav-link text-light" href="mymessages">
                 Mes Messages
               </a>
-            </li>*/}
+            </li>}*/}
             <li className="nav-item">
-              <a className="nav-link" href="myrequests">
+              <a className="nav-link text-light" href="myrequests">
                 Mes Demandes
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="mycomments">
+              <a className="nav-link text-light" href="mycomments">
                 Commentaires
               </a>
             </li>
 
             <li className="d-flex">
-              <button className="d-flex nav-link btn btn-outline btn-danger btn-sm" onClick={logout}>
+              <button className="d-flex nav-link text-dark btn btn-warning btn-sm ms-2 " onClick={logout}>
                 Se déconnecter
               </button>
             </li>
           </ul>
         </div>
       </div>
-    </nav>
+    </div>
   )
 }
 export default PartnerNavbar
