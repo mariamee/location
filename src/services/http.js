ec71c41a2cd174949d610560a82ba454b79637e2
mariamee <mariameelalaoui@gmail.com>
2022-05-25 18:41:14 +0100

    add laravel heroku

--- src/services/http.js
+++ src/services/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8000/api'
+axios.defaults.baseURL = 'https://calm-springs-37850.herokuapp.com/api'
 
 export default {
   get: axios.get,
